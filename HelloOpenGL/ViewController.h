ce54968b0c3e4ff53bcf46f789cccddbca037c1d
xshadow90 <xshadow1990@gmail.com>
2015-05-25 15:19:39 -0700

    Initial Commit

---
+++ HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by Shaohan Xu on 5/25/15.
+//  Copyright (c) 2015 Shaohan Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloOpenGL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloOpenGL
+//
+//  Created by Shaohan Xu on 5/25/15.
+//  Copyright (c) 2015 Shaohan Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
