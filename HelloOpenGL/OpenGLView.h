091ee2f73011ddd720abc71caba02c3918b7b06f
xshadow90 <xshadow1990@gmail.com>
2015-05-29 01:30:02 -0700

    original

---
+++ HelloOpenGL/OpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLView.h
+//  HelloOpenGL
+//
+//  Created by Shaohan Xu on 5/25/15.
+//  Copyright (c) 2015 Shaohan Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLView : UIView
+
+@end
---
+++ HelloOpenGL/OpenGLView.m
@@ -0,0 +1,21 @@
+//
+//  OpenGLView.m
+//  HelloOpenGL
+//
+//  Created by Shaohan Xu on 5/25/15.
+//  Copyright (c) 2015 Shaohan Xu. All rights reserved.
+//
+
+#import "OpenGLView.h"
+
+@implementation OpenGLView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
